4a93be129c15ecd4a43b9406a68497c167619708
Kaua <kauaoliveira18062007@gmail.com>
2026-05-11 19:15:47 -0300

    feat: add TailWind CSS and Cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,56 @@ import { Card } from "./Card"
 
 function App() {
   return (
-    <main style={{padding: '40px', fontFamily: 'sans-serif'}}>
-      
-      <h1>Choose your plan</h1>
+    <main 
+      className={`
+        min-h-screen
+        bg-gray-100
+        flex
+        flex-col
+        items-center
+        justify-between-center
+        p-24
 
-      <div style={{display: 'flex'}}>
-        <Card
-          title="Gold Plan"
+        `}
+    >
+      
+      <h1
+        className="
+            text-3xl
+            font-bold
+            text-centers
+            text-gray-800
+          "
+      >Choose your plan</h1>
+
+      <div
+        className="
+          flex
+          flex-wrap
+          justify-between
+          mt-10
+          max-w-2xl
+          mx-auto
+          h-full
+          items-center
+        "
+      >
+        <Card variant="silver"
+          title="SILVER"
+          price="$ 49,90"
+          benefits={["Diary content", "Monthly mentoring"]}
+        />
+
+        <Card variant="gold"
+          title="GOLD"
           price="$ 99,90"
           benefits={["Diary content", "Monthly mentoring", "VIP group"]}
+        />
+
+        <Card variant="bronze"
+          title="BRONZE"
+          price="$ 24,90"
+          benefits={["Diary content"]}
         />    
       </div>
     </main>
