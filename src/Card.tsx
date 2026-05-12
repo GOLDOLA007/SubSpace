0a33fa077b2d569608dff3a682c2e06cc8670595
Kaua <kauaoliveira18062007@gmail.com>
2026-05-12 13:22:18 -0300

    style: fix buttons position using h-full

--- src/Card.tsx
+++ src/Card.tsx
@@ -9,9 +9,9 @@ interface CardProps{
 export function Card(Props: CardProps){
 
     const styles = {
-        bronze: "border-gray-800 hover:border-orange-800 text-orange-400 hover:shadow-[0_0_15px_rgba(234,88,12,0.2)] w-48 min-h-48",
-        silver: "border-gray-800 hover:border-gray-600 text-white hover:shadow-[0_0_15px_rgba(148,163,184,0.1)] w-48 min-h-48",
-        gold: "border-gray-800 hover:border-yellow-800 text-yellow-400 hover:shadow-[0_0_20px_rgba(202,138,4,0.2)] w-64 min-h-64"
+        bronze: "border-gray-800 hover:border-orange-800 text-orange-400 hover:shadow-[0_0_15px_rgba(234,88,12,0.2)] w-48 h-48",
+        silver: "border-gray-800 hover:border-gray-600 text-white hover:shadow-[0_0_15px_rgba(148,163,184,0.1)] w-48 h-48",
+        gold: "border-gray-800 hover:border-yellow-800 text-yellow-400 hover:shadow-[0_0_20px_rgba(202,138,4,0.2)] w-64 h-64"
     }
 
     const buttonStyles = {
@@ -29,71 +29,87 @@ export function Card(Props: CardProps){
                 text-center 
                 shadow-md 
                 bg-zinc-900
+                w-48
+                h-48
                 ${styles[Props.variant]}
             `}
         >
-
             <div
                 className={`
+                    h-full
+                    flex
                     flex-col
-                    mb-4
-                    justify-center
-                    items-center
-                `}
-            >
-                <h2 
-                className={`
-                    text-xl
-                    font-bold
-                    mb-2
-                `}
-                >{Props.title}</h2>
-            </div>
-            
-            <div
-                className={`
-                    flex-col
-                    mb-4
-                    justify-center
-                    items-center
+                    justify-between
                     `}
             >
-                    <ul 
+
+            
+                <div
                     className={`
-                        text-left
-                        list-disc
-                        list-inside
-                        mb-6
-                        space-y-1
-                        text-zinc-400
+                        flex-col
+                        justify-center
+                        items-center
                     `}
                 >
-                    {Props.benefits.map((item) => (
-                        <li key={item}
-                            className={`
-                                text-sm
-                            `}
-                        >{item}</li>
-                    ))}
-                </ul>
-            </div>
-            
-            <div>
-                <button 
-                className={`
-                        mt-auto
-                        w-full
-                        ${buttonStyles[Props.variant]}
-                        duration-300
-                        text-white
+                    <h2 
+                    className={`
+                        text-xl
                         font-bold
-                        py-2
-                        px-4
-                        border-none
-                        rounded
-                        shadow-md
-                `}
-                >{Props.price}</button>
+                        mb-2
+                    `}
+                    >{Props.title}</h2>
+                </div>
+                
+                <div
+                    className={`
+                        flex-col
+                        justify-center
+                        items-center
+                        `}
+                >
+                        <ul 
+                        className={`
+                            text-left
+                            list-disc
+                            list-inside
+                            mb-6
+                            space-y-1
+                            text-zinc-400
+                        `}
+                    >
+                        {Props.benefits.map((item) => (
+                            <li key={item}
+                                className={`
+                                    text-sm
+                                `}
+                            >{item}</li>
+                        ))}
+                    </ul>
+                </div>
+                
+                <div
+                    className={`
+                        flex
+                        justify-center
+                        items-end
+                        h-1/3
+                    `}
+                >
+                    <button 
+                    className={`
+                            w-full
+                            ${buttonStyles[Props.variant]}
+                            duration-300
+                            text-white
+                            font-bold
+                            py-2
+                            px-4
+                            border-none
+                            rounded
+                            shadow-md
+                    `}
+                    >{Props.price}</button>
+                </div>
             </div>
             
         </div>
